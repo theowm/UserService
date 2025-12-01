60b265a040eada1c27358b8cc2ea3127ee514e2d
theowm <theo.winter.milatovic@gmail.com>
2025-12-01 10:10:50 +0100

    Updated data.sql uuid for users

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,8 +3,8 @@ INSERT INTO users (uuid, name, username) VALUES
 ('d4733399-2251-4ba2-8bab-3035fe884edc', 'Jane Smith', 'jane');
 
 INSERT INTO likes (uuid, user_uuid, media_uuid, media_type) VALUES
-('aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', '11111111-1111-1111-1111-111111111111', '99999999-9999-9999-9999-999999999999', 'MUSIC'),
-('bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb', '22222222-2222-2222-2222-222222222222', '88888888-8888-8888-8888-888888888888', 'VIDEO');
+('aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', '57b038ed-fb36-4b39-8748-5fafe89bf2b0', '99999999-9999-9999-9999-999999999999', 'MUSIC'),
+('bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb', 'd4733399-2251-4ba2-8bab-3035fe884edc', '88888888-8888-8888-8888-888888888888', 'VIDEO');
 
 INSERT INTO dislikes (uuid, user_uuid, media_uuid, media_type) VALUES
-('cccccccc-cccc-cccc-cccc-cccccccccccc', '11111111-1111-1111-1111-111111111111', '77777777-7777-7777-7777-777777777777', 'PODCAST');
+('cccccccc-cccc-cccc-cccc-cccccccccccc', '57b038ed-fb36-4b39-8748-5fafe89bf2b0', '77777777-7777-7777-7777-777777777777', 'PODCAST');
